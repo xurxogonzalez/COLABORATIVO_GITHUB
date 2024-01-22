c0d16bfa86bd4f6d9b6abc8f8b98d8e9ac115ee2
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2024-01-22 08:51:41 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+function esPrimo(numero) {
+    if (numero <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 function sumar(a, b) {
     return a + b;
+}
+
+
+function sumarYVerificarPrimo(a, b) {
+    const suma = sumar(a, b);
+    return esPrimo(suma);
 }
