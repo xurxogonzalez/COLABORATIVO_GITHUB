29af2d45795c126acab429f691b68bcfce71b924
SaraRodoCalvo <srodcal.appventurers@gmail.com>
2024-01-22 09:32:43 +0100

    Solución ao exercicio esPrimo

---
+++ sararodo-es-primo.js
@@ -0,0 +1,11 @@
+function esPrimo (numero) {
+    const primo = (numero % numero == 0)
+    const primo2 = (numero % 1 == 0)
+    
+    if (primo, primo2 == 0) {
+      alert('É número primo')
+    } else {
+      alert('Non é número primo')
+    }
+  }
+  
